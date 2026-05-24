15961da04f09c45d074bddd1903e1e83b5469f8b
keynis <keydejun@gmail.com>
2026-05-24 21:15:08 +0200

    Quitar opcion de imagenes ya que sale en edit

--- backend/MvcWearIt/MvcWearIt/Controllers/ProductosController.cs
+++ backend/MvcWearIt/MvcWearIt/Controllers/ProductosController.cs
@@ -201,48 +201,6 @@ namespace MvcWearIt.Controllers
             return _context.Productos.Any(e => e.Id == id);
         }
 
-        // GET: Productos/CambiarImagen/5
-        public async Task<IActionResult> CambiarImagen(int? id)
-        {
-            if (id == null) return NotFound();
-
-            var producto = await _context.Productos
-                .Include(p => p.Categoria)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (producto == null) return NotFound();
-
-            return View(producto);
-        }
-
-        // POST: Productos/CambiarImagen/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CambiarImagen(int? id, string imagenUrl)
-        {
-            if (id == null) return NotFound();
-
-            var producto = await _context.Productos.FindAsync(id);
-            if (producto == null) return NotFound();
-
-            if (string.IsNullOrWhiteSpace(imagenUrl))
-                return NotFound();
-
-            producto.Imagen = imagenUrl;
-
-            try
-            {
-                _context.Update(producto);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductoExists(producto.Id)) return NotFound();
-                else throw;
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
         [HttpGet]
         public async Task<IActionResult> ObtenerPorJuego(int juegoId)
         {
